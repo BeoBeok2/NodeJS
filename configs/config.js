fd659f3e547af522e206b74fad995d75a9d3fac7
Văn Bảo <beobeok2@gmail.com>
2024-04-02 23:21:27 +0700

    first commit

---
+++ configs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SECRET_KEY: 'TUNG123',
+    EXPIRE_JWT: '1d',
+    hostName: 'localhost:3000',
+    Host: "sandbox.smtp.mailtrap.io",
+    Port: 25,
+    Username: "ee7e471f961a42",
+    Password: "fff34053437310"
+}
